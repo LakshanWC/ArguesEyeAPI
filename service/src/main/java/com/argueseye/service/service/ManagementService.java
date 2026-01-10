94fa45381b96e0fc84342c12f9d152465e525fab
LakshanWC <lakshanwc@gmail.com>
2026-01-10 19:04:47 +0530

    Update ManagementService.java

--- service/src/main/java/com/argueseye/service/service/ManagementService.java
+++ service/src/main/java/com/argueseye/service/service/ManagementService.java
@@ -1,6 +1,7 @@
 package com.argueseye.service.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class ManagementService {
 
     @Autowired
